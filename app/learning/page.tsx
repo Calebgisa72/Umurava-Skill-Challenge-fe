de974e3bd5c8706bb1d2145e30e5374aba4f78d8
mauriceirimaso <irimasomaurice@gmail.com>
2025-01-26 17:22:01 +0200

    finished challengepage

---
+++ app/learning/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './style.css'
+
+const Learning = () => {
+  return (
+    <>
+    <div className='grand-container'>
+
+         <div className='heading-div'>
+
+         </div>
+          <div className='student-div'>
+      
+          </div>
+      </div>
+    </>
+   
+  )
+}
+
+export default Learning
